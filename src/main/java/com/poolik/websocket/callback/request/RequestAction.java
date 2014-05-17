47f9c704df6c38dba71acafdd86ebd52a4b77497
Tõnis Pool <tonis.pool@gmail.com>
2014-05-17 12:45:36 +0300

    Can now retrieve the underlying session of WebSocketRequest, do not retrieve the handler in handleRequest

--- src/main/java/com/poolik/websocket/callback/request/RequestAction.java
+++ src/main/java/com/poolik/websocket/callback/request/RequestAction.java
@@ -1,7 +1,7 @@
 package com.poolik.websocket.callback.request;
 
+import com.poolik.websocket.callback.RequestHandlerResolver;
 import com.poolik.websocket.callback.WebSocketRequestHandler;
-import com.poolik.websocket.callback.WebSocketResponse;
 import com.poolik.websocket.callback.filter.WebSocketFilter;
 
 import java.util.Collection;
@@ -10,16 +10,17 @@ import java.util.concurrent.Callable;
 public final class RequestAction implements Callable<Response> {
   private final Request request;
   private final Collection<WebSocketFilter> filters;
-  private final WebSocketRequestHandler handler;
+  private final RequestHandlerResolver requestHandlerResolver;
 
-  public RequestAction(Request request, Collection<WebSocketFilter> filters, WebSocketRequestHandler handler) {
+  public RequestAction(Request request, Collection<WebSocketFilter> filters, RequestHandlerResolver requestHandlerResolver) {
     this.request = request;
     this.filters = filters;
-    this.handler = handler;
+    this.requestHandlerResolver = requestHandlerResolver;
   }
 
   @Override
   public Response call() throws Exception {
+    WebSocketRequestHandler handler = requestHandlerResolver.getHandlerFor(request.url, request.type);
     Exception exception = applyFilters();
     if (exception != null) return new ErrorResponse(request.callbackId, exception);
     return new Response(request.callbackId, handler.handle(request));
--- src/main/java/com/poolik/websocket/callback/request/ResponseAction.java
+++ src/main/java/com/poolik/websocket/callback/request/ResponseAction.java
@@ -5,30 +5,26 @@ import com.poolik.websocket.callback.util.F;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.websocket.Session;
-
 public final class ResponseAction implements F.Action<F.Promise<Response>> {
 
   private static final Logger log = LoggerFactory.getLogger(ResponseAction.class);
   private static final Gson gson = new Gson();
-  private final Session session;
   private final Request request;
 
-  public ResponseAction(Session session, Request request) {
-    this.session = session;
+  public ResponseAction(Request request) {
     this.request = request;
   }
 
   @Override
   public void invoke(F.Promise<Response> result) {
-    if (session.isOpen()) {
+    if (request.session.isOpen()) {
       log.trace("Sending response to " + request.callbackId);
       Response requestResponse = result.getOrNull();
       try {
-        if (requestResponse != null) session.getAsyncRemote().sendText(gson.toJson(requestResponse));
+        if (requestResponse != null) request.session.getAsyncRemote().sendText(gson.toJson(requestResponse));
         else {
           log.error("Request " + request.type + " '" + request.url + "' failed with: ", result.getException());
-          session.getAsyncRemote().sendText(gson.toJson(new ErrorResponse(request.callbackId, result.getException())));
+          request.session.getAsyncRemote().sendText(gson.toJson(new ErrorResponse(request.callbackId, result.getException())));
         }
       } catch (Throwable e) {
         log.error("Failed to send response: " + (requestResponse != null ? requestResponse.toString() : result.getException()), e);
